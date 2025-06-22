a3cf2c4cce0f71ec583d593260ab563666284004
anja669 <popovicanja972@gmail.com>
2025-06-22 15:15:17 +0200

    Added reviews for admin

---
+++ src/main/java/com/example/IT355_PZ01_5363/controller/admin/AllReviewsController.java
@@ -0,0 +1,21 @@
+package com.example.IT355_PZ01_5363.controller.admin;
+
+import com.example.IT355_PZ01_5363.repository.DB;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AllReviewsController {
+    private final DB db;
+
+    public AllReviewsController(DB db) {
+        this.db = db;
+    }
+
+    @GetMapping("/admin/reviews")
+    public String showReviews(Model model){
+        model.addAttribute("reviews", db.getAllReviews());
+        return "admin/reviews";
+    }
+}

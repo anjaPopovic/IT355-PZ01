c13834b0a7a58624d9ba79584f343064d8b3b6c8
anja669 <popovicanja972@gmail.com>
2025-06-23 19:45:40 +0200

    Separated code from home file

---
+++ src/main/java/com/example/IT355_PZ01_5363/controller/client/TreatmentController.java
@@ -0,0 +1,23 @@
+package com.example.IT355_PZ01_5363.controller.client;
+
+import com.example.IT355_PZ01_5363.service.TreatmentService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TreatmentController {
+
+    private final TreatmentService treatmentService;
+
+    public TreatmentController(TreatmentService treatmentService) {
+        this.treatmentService = treatmentService;
+    }
+
+    @GetMapping("/treatments")
+    public String showHome(Model model){
+        model.addAttribute("treatments", treatmentService.getAllTreatments());
+        return "client/treatmentForm";
+    }
+
+}
